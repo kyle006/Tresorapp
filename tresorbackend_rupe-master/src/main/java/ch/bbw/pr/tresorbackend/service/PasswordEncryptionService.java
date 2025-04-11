4612871a9e0276cafaf501d6f0797206d5331d2c
kyle006 <kyle.jeremy.meier@gmail.com>
2025-04-11 16:05:35 +0200

    Passwort hash gemacht

--- tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/service/PasswordEncryptionService.java
+++ tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/service/PasswordEncryptionService.java
@@ -1,21 +1,24 @@
 package ch.bbw.pr.tresorbackend.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
  * PasswordEncryptionService
- * @author Peter Rutschmann
+ * @author Kyle Meier
  */
 @Service
 public class PasswordEncryptionService {
-   //todo ergänzen!
-
-   public PasswordEncryptionService() {
-      //todo anpassen!
-   }
+   private final String pepper = "staticPepperValue";
 
    public String hashPassword(String password) {
-      //todo anpassen!
-      return password;
+      // BCrypt automatically handles salting
+      String saltedPassword = password + pepper;
+      return new BCryptPasswordEncoder().encode(saltedPassword);
+   }
+
+   public boolean checkPassword(String rawPassword, String encodedPassword) {
+      String saltedPassword = rawPassword + pepper;
+      return new BCryptPasswordEncoder().matches(saltedPassword, encodedPassword);
    }
 }
